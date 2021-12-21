c03e563f99b94a9b4e696ceb07d9b3f813c35be5
Kevin Kabore <kevin.s.kabore@gmail.com>
2021-12-21 11:35:21 -0500

    feat: adds check membership nft

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react'
 import {useWeb3} from '@3rdweb/hooks'
+import {ThirdwebSDK} from '@3rdweb/sdk'
 
+// instatiate the ThirdwebSDK on Rinkeby.
+const sdk = new ThirdwebSDK('rinkeby')
+// grab a reference to the ERC-1155 contract.
+const bundleDropModule = sdk.getBundleDropModule(
+  '0xE0d9D544DC499f62519dA2614D3A842782Ed1FCf',
+)
 export const LandingContainer: React.FC = ({children}) => (
   <div className="landing">{children}</div>
 )
@@ -8,6 +15,33 @@ const Welcome: React.FC = () => <h2>Welcome to the AfroBuildersDAO</h2>
 
 export const App = () => {
   const {connectWallet, address, error, provider} = useWeb3()
+  // State variable for us to know if user has our NFT.
+  const [isNFTClaimed, setIsNFTClaimed] = React.useState(false)
+
+  React.useEffect(() => {
+    if (!address) {
+      return
+    }
+    // Check if the user has the NFT by using bundleDropModule.balanceOf
+    const getIsNFTClaimed = async () => {
+      await bundleDropModule.balanceOf(address, [0]).then(
+        balance => {
+          // nft claimed if balance > 0
+          if (balance.gt(0)) {
+            setIsNFTClaimed(true)
+            console.log('🌟 this user has a membership NFT!')
+          } else {
+            setIsNFTClaimed(false)
+            console.log("😭 this user doesn't have a membership NFT.")
+          }
+        },
+        error => {
+          console.error('error getting nft balance', error)
+        },
+      )
+    }
+    getIsNFTClaimed()
+  }, [address])
 
   if (!address) {
     return (
@@ -23,7 +57,11 @@ export const App = () => {
   return (
     <LandingContainer>
       <Welcome />
-      👀 wallet connected, now what!
+      {isNFTClaimed ? (
+        <p>👀 user has a membership nft, now what!</p>
+      ) : (
+        <p>Claim nft!</p>
+      )}
     </LandingContainer>
   )
 }
