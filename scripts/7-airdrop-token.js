0baf21b85854e09af2eae22c3273abe5583b78e8
Kevin Kabore <kevin.s.kabore@gmail.com>
2021-12-24 19:35:24 -0500

    feat: airdrop to ERC-1155 token holders

---
+++ scripts/7-airdrop-token.js
@@ -0,0 +1,47 @@
+import {ethers} from 'ethers'
+import sdk from './1-initialize-sdk.js'
+
+// address of the ERC-1155 membership NFT contract
+const bundleDropModule = sdk.getBundleDropModule(
+  '0xE0d9D544DC499f62519dA2614D3A842782Ed1FCf',
+)
+
+// ERC-20 token contract address
+const tokenModule = sdk.getTokenModule(
+  '0xbC1b9B1Fd398753B46e5F29EF22707eE3Dc0Ca98',
+)
+
+;(async () => {
+  try {
+    // Grab all the addresses of people who own our membership NFT, which has
+    // a tokenId of 0.
+    const walletAddresses = await bundleDropModule.getAllClaimerAddresses('0')
+    if (!walletAddresses.length) {
+      console.log(
+        'No NFTs have been claimed yet, maybe get some friends to claim your free NFTs!',
+      )
+      process.exit(0)
+    }
+
+    const airdropTargets = walletAddresses.map(address => {
+      // Pick a random # between 1000 and 10000.
+      const randomAmount = Math.floor(Math.random() * (10000 - 1000 + 1) + 1000)
+      console.log('✅ Going to airdrop', randomAmount, 'tokens to', address)
+
+      // setup the target
+      const airdropTarget = {
+        address,
+        amount: ethers.utils.parseUnits(randomAmount.toString(), 18), // we need 18 decimal placees!
+      }
+      return airdropTarget
+    })
+    // Call transferBatch on all our airdrop targets.
+    console.log('🌈 Starting airdrop...')
+    await tokenModule.transferBatch(airdropTargets)
+    console.log(
+      '✅ Successfully airdropped tokens to all the holders of the NFT!',
+    )
+  } catch (e) {
+    console.error('Failed to airdrop tokens', e)
+  }
+})()
